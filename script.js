4f2d93bc2e3b4a40e33cc918ce4bf4516d88f0f1
mkeacs <109988238+mkeacs@users.noreply.github.com>
2023-04-17 16:40:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var output = document.querySelector('#output');
 
 function cript(){
     var texto = input.value.toLowerCase();
-    var result = texto.replace(/[eÉÈÊË]/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat").replace(/a/g, "ai");
+    var result = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
 
     document.getElementById('output').innerHTML = '<textarea readonly id="input-text">' + result + '</textarea>' + '<button class="btn-cop" id="copiar" onclick="copiar()">Copiar<button>'
 
@@ -27,15 +27,3 @@ function copiar() {
     document.execCommand('copy');
     alert("Texto copiado!");
 }  
-
-/*function removeAcento (text)
-{       
-    text = text.toLowerCase();                                                         
-    text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
-    text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
-    text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
-    text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
-    text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
-    text = text.replace(new RegExp('[Ç]','gi'), 'c');
-    return text;                 
-}*/
